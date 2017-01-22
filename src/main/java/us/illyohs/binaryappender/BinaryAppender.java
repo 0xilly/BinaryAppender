7682ad282d44889dce965f0f190f55f18562aaae
Anthony Anderson <lordillyohs@gmail.com>
2017-01-21 22:03:40 -0500

    inital commit
    Signed-off-by: Anthony Anderson <lordillyohs@gmail.com>

---
+++ src/main/java/us/illyohs/binaryappender/BinaryAppender.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2016, Anthony Anderson(Illyohs)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice, this
+ *        list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *        this list of conditions and the following disclaimer in the documentation
+ *        and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ */
+package us.illyohs.binaryappender;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+
+public class BinaryAppender {
+
+
+    /**
+     * This function serves to append the binary
+     * @param appendable the file to append to the binary
+     * @param targetBin the target binary
+     */
+    public static void appendBinary(File appendable, File targetBin)
+    {
+        try {
+            FileInputStream in   = new FileInputStream(appendable);
+            FileOutputStream out = new FileOutputStream(targetBin, true);
+            out.write(IOUtils.toByteArray(in));
+            out.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/us/illyohs/binaryappender/launch/Launch.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2016, Anthony Anderson(Illyohs)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice, this
+ *        list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *        this list of conditions and the following disclaimer in the documentation
+ *        and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ */
+package us.illyohs.binaryappender.launch;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import us.illyohs.binaryappender.BinaryAppender;
+
+import java.io.File;
+
+import static us.illyohs.binaryappender.BinaryAppender.appendBinary;
+
+public class Launch {
+
+    @Parameter(names = {"--targetbinary", "-t"}, required = true)
+    static String targetBin;
+
+    @Parameter(names = {"--append", "-a"}, required = true)
+    static String targetJar;
+
+    public static void main(String... args)
+    {
+        BinaryAppender appender = new BinaryAppender();
+        new JCommander(appender, args);
+        System.out.println(targetBin);
+        System.out.println(targetJar);
+
+        File binPath = new File(targetBin);
+        File jarPath = new File(targetJar);
+
+        appendBinary(jarPath,binPath);
+
+    }
+}
